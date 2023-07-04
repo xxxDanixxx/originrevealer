7b6ad7e5bcedc79f2d799c4806ab09068152f45f
xxxDanixxx <136775032+xxxDanixxx@users.noreply.github.com>
2023-07-04 16:04:04 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,87 @@
+# OriginRevealer
+
+originrevealer is a Tool for extracting possible Origin IPs given a favicon url
+
+## Installation And Setup
+
+1. clone the project :
+```
+git clone <link>
+```
+
+2. Prepare and activate the virtual environment :
+```
+$ python3 -m venv myenv
+$ source myenv/bin/activate
+```
+
+3. Install requirements :
+```
+(myenv) $ pip install -r requirements.txt
+```
+
+## Usage
+
+```
+                                      ,,~~--___---,
+                                     /            .~,
+                               /  _,~             )
+                              (_-(~)   ~, ),,,(  /'
+                                /  .~`' ||     \ |
+                               /_,/     ||      ||
+                         ~~~~~~~~~~~~~~~W`~~~~~~W`~~~~~~~~~
+     OriginRevealer
+     Powered By CriminalIP : https://www.criminalip.io/
+
+usage: originrevealer.py [-h] [--init API_KEY] [-t TARGET]
+
+Tool for extracting possible Origin IPs given a favicon url
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --init API_KEY        Initialize and store the API key
+  -t TARGET, --target TARGET
+                        Favicon target url
+```
+
+### API KEY
+To make use of the tool, you will need to acquire an API key from CriminalIP. Once you have obtained the key, you can authenticate and access the CriminalIP API seamlessly.
+
+```
+python3 originrevealer.py --init a123b123
+                                      ,,~~--___---,
+                                     /            .~,
+                               /  _,~             )
+                              (_-(~)   ~, ),,,(  /'
+                                /  .~`' ||     \ |
+                               /_,/     ||      ||
+                         ~~~~~~~~~~~~~~~W`~~~~~~W`~~~~~~~~~
+     OriginRevealer
+     Powered By CriminalIP : https://www.criminalip.io/
+
+CriminalIP API key initialized and stored.
+```
+### Get Origin IPs :
+```
+$ python3 originrevealer.py -t https://www.facebook.com/favicon.ico
+                                      ,,~~--___---,
+                                     /            .~,
+                               /  _,~             )
+                              (_-(~)   ~, ),,,(  /'
+                                /  .~`' ||     \ |
+                               /_,/     ||      ||
+                         ~~~~~~~~~~~~~~~W`~~~~~~W`~~~~~~~~~
+     OriginRevealer
+     Powered By CriminalIP : https://www.criminalip.io/
+
+157.240.212.35
+....
+```
+
+
+
+### Notes
+
+- Exercise caution when using the tool, as it may retrieve assets that do not belong to the target or false positive .
+
+- To ensure proper functionality of the tool, it relies on CriminalIP. If you encounter any errors during execution, it may indicate that you have exceeded the query limit associated with your API key. visit the [CriminalIP](https://www.criminalip.io/en/pricing) website for more informations
---
+++ originrevealer.py
@@ -0,0 +1,130 @@
+import requests
+import mmh3
+import codecs
+import argparse
+from tld import get_tld
+import configparser
+
+CONFIG_FILE = 'config.ini'
+
+
+def banner():
+    print("""
+                                      ,,~~--___---,
+                                     /            .~,
+                               /  _,~             )
+                              (_-(~)   ~, ),,,(  /'
+                                /  .~`' ||     \ |
+                               /_,/     ||      ||
+                         ~~~~~~~~~~~~~~~W`~~~~~~W`~~~~~~~~~
+     OriginRevealer                    
+     Powered By CriminalIP : https://www.criminalip.io/
+    """)
+
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Tool for extracting possible Origin IPs given a favicon url")
+    parser.add_argument('--init', metavar='API_KEY', help='Initialize and store the API key')
+    parser.add_argument("-t", "--target", help="Favicon target url", type=str)
+    return parser.parse_args()
+
+
+def initialize_api_key(api_key):
+    config = configparser.ConfigParser()
+    config['DEFAULT'] = {'API_KEY': api_key}
+
+    with open(CONFIG_FILE, 'w') as configfile:
+        config.write(configfile)
+
+    print("CriminalIP API key initialized and stored.")
+
+def get_api_key():
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE)
+
+    if 'DEFAULT' in config and 'API_KEY' in config['DEFAULT']:
+        return config['DEFAULT']['API_KEY']
+
+    return None
+
+
+# this function will calculate favicon hash value 
+def calculate_hash(url):
+    response = requests.get(url)
+
+    favicon = codecs.encode(response.content,"base64")
+    hash = mmh3.hash(favicon)
+    hex_val=hex(hash)
+    if hex_val.startswith('-'):
+        return "-"+hex_val[3:]
+    else:
+        return hex_val[2:]
+
+# this function will extract IPs based on favicon hash
+def retrieve_ip_fh(faviconhash,offset=0):
+    url = "https://api.criminalip.io/v1/banner/search"
+    headers = {
+        "x-api-key": api_key
+    }
+
+    params = {
+        "query": f"favicon:{faviconhash}",
+        "offset": offset
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    data = response.json()["data"]["result"]
+    ip_addresses = sorted(set(item["ip_address"] for item in data))
+
+    for ip_address in ip_addresses:
+        print(ip_address)
+
+
+# this function will retrieve IPs based on tld
+def retrieve_ip(url,offset=0):
+    domain=get_tld(url,as_object=True).fld
+
+    url = "https://api.criminalip.io/v1/banner/search"
+    headers = {
+        "x-api-key": api_key
+    }
+
+    params = {
+        "query": f"ssl_subject_common_name:{domain}",
+        "offset": offset
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    data = response.json()["data"]["result"]
+    ip_addresses = sorted(set(item["ip_address"] for item in data))
+
+    for ip_address in ip_addresses:
+        print(ip_address)
+
+
+
+
+if __name__ == '__main__':
+    banner()
+
+    args = parse_arguments()
+
+
+    # initialize your api key
+    if args.init:
+        initialize_api_key(args.init)
+
+    try:
+        if args.target:
+            if get_api_key() is None or not get_api_key().strip():
+                print("please initialise your API key")
+            else:
+                api_key=get_api_key()
+            
+                # retrieving IPs using favicon hash trick
+                retrieve_ip_fh(calculate_hash(args.target))
+                # retrieve IPs using the domain name
+                retrieve_ip(args.target)
+    except KeyError:
+        print("Error: Wrong api key or insufficient credit")
